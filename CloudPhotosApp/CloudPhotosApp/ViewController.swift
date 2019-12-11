0111a25afba737ae043a1feb8239e32876a7f062
Mark <mkriegsies1@live.maryville.edu>
2019-12-10 21:55:50 -0600

    initial commit

---
+++ CloudPhotosApp/CloudPhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudPhotosApp
+//
+//  Created by Mark Kriegsies on 12/10/19.
+//  Copyright © 2019 Mark Kriegsies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

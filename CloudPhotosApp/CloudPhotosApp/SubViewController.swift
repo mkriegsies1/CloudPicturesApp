10fa73768436618de6d92e10798a1db433854c3d
Mark <mkriegsies1@live.maryville.edu>
2019-12-11 18:53:28 -0600

    messed up, got errors, starting from scratch

---
+++ CloudPhotosApp/CloudPhotosApp/SubViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SubViewController.swift
+//  CloudPhotosApp
+//
+//  Created by Mark Kriegsies on 12/11/19.
+//  Copyright © 2019 Mark Kriegsies. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SubViewController: UIViewController, UIPageViewControllerDelegate, UIImagePickerControllerDelegate {
+    
+    
+    
+}
+
--- CloudPhotosApp/CloudPhotosApp/ViewController.swift
+++ CloudPhotosApp/CloudPhotosApp/ViewController.swift
@@ -5,14 +5,44 @@
 //  Created by Mark Kriegsies on 12/10/19.
 //  Copyright © 2019 Mark Kriegsies. All rights reserved.
 //
+// Sources: "Davids Videos" https://www.youtube.com/watch?v=wDzk5KDe6Uw
+//
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController /*UIImagePickerController, UIImagePickerControllerDelegate, UINavigationControllerDelegate */ {
+    
+    //Outlets and Actions
+    
+
+    
+    //Picking an Image
+    /* func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        if let image = info[.originalImage] as? UIImage
+        {
+            myImageView.image = image
+            
+        }
+        else
+        {
+            //Error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    } */
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //self.myImageView.contentMode = .scaleToFill
     }
 
 

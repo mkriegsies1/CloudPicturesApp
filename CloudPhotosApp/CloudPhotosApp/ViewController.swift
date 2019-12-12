77b36e79188c87145bf7927778ba3281779d57c5
Mark <mkriegsies1@live.maryville.edu>
2019-12-11 21:42:15 -0600

    nother commit

--- CloudPhotosApp/CloudPhotosApp/ViewController.swift
+++ CloudPhotosApp/CloudPhotosApp/ViewController.swift
@@ -5,25 +5,39 @@
 //  Created by Mark Kriegsies on 12/10/19.
 //  Copyright © 2019 Mark Kriegsies. All rights reserved.
 //
-// Sources: "Davids Videos" https://www.youtube.com/watch?v=wDzk5KDe6Uw
+// Sources: "Davids Videos" on YouTube https://www.youtube.com/watch?v=wDzk5KDe6Uw
 //
 
 import UIKit
 
 
 
-class ViewController: UIViewController /*UIImagePickerController, UIImagePickerControllerDelegate, UINavigationControllerDelegate */ {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //Outlets and Actions
+    @IBOutlet var imageView: UIImageView!
     
-
+    
+    @IBAction func chooseImageButton(_ sender: Any)
+    {
+  
+    let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = .photoLibrary
+        //image.sourceType = .camera
+        image.allowsEditing = false
+    }
+    
+    @IBAction func uploadToCloudButton(_ sender: Any) {
+    }
+    
     
     //Picking an Image
-    /* func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
     {
         if let image = info[.originalImage] as? UIImage
         {
-            myImageView.image = image
+            imageView.image = image
             
         }
         else
@@ -33,7 +47,7 @@ class ViewController: UIViewController /*UIImagePickerController, UIImagePickerC
         
         self.dismiss(animated: true, completion: nil)
         
-    } */
+    }
     
     
     
